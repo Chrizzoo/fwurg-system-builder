ea731dfe95a403fd22791bc6d34337afff545349
Brend Wanders <b.wanders@xs4all.nl>
2012-07-09 17:30:05 +0000

    Adds Orbit type.

--- model.js
+++ model.js
@@ -38,41 +38,57 @@ Feature.prototype.resources = function(system, orbit, orbital) {
 	}
 }
 
+/**
+ * Adds the feature aspect to a type.
+ */
+var featurify = function(type) {
+	type.prototype.hasFeature = function(f){return f in this._features;};
+	type.prototype.addFeature = function(f){this._features[f] = true; return this;};
+	type.prototype.removeFeature = function(f){delete this._features[f]; return this;};
+
+	var x = type.prototype.init;
+	type.prototype.init = function() {
+		x.apply(this);
+		this._features = {};
+	}
+
+	/**
+	 * Returns a list of features, or sets the list of features.
+	 */
+	type.prototype.features = function(fs) {
+		if(typeof fs == 'undefined') {
+			var r=[];
+			for(var f in this._features) r.push(Feature.get(f));
+			return r;
+		} else {
+			this._features = {};
+			for(var k in fs) this.addFeature(fs[k]);
+			return this;
+		}
+	}
+}
+
 
 /**
  * An orbital
  */
 var Orbital = function(orbit) {
 	this._orbit = orbit;
-	this._features = {};
 	this._name = "";
 	this._image = "";
 	this._description = "";
+	this.init();
 }
 
-Orbital.prototype.name = function(v){if(typeof v=='undefined')return this._name; this._name=v; return this;}
-Orbital.prototype.image = function(v){if(typeof v=='undefined')return this._image; this._image=v; return this;}
-Orbital.prototype.description = function(v){if(typeof v=='undefined')return this._description; this._description=v; return this;}
+Orbital.prototype.init = function(){};
+
+featurify(Orbital);
+
+Orbital.prototype.name = function(v){if(typeof v=='undefined')return this._name; this._name=v; return this;};
+Orbital.prototype.image = function(v){if(typeof v=='undefined')return this._image; this._image=v; return this;};
+Orbital.prototype.description = function(v){if(typeof v=='undefined')return this._description; this._description=v; return this;};
 Orbital.prototype.orbit = function(){return this._orbit;};
 
-Orbital.prototype.hasFeature = function(f){return f in this._features;}
-Orbital.prototype.addFeature = function(f){this._features[f] = true; return this;}
-Orbital.prototype.removeFeature = function(f){delete this._features[f]; return this;}
-
-/**
- * Returns a list of features, or sets the list of features.
- */
-Orbital.prototype.features = function(fs) {
-	if(typeof fs == 'undefined') {
-		var r=[];
-		for(var f in this._features) r.push(Feature.get(f));
-		return r;
-	} else {
-		this._features = {};
-		for(var k in fs) this.addFeature(fs[k]);
-		return this;
-	}
-}
 
 /**
  * Determines the resources of this orbital.
@@ -91,8 +107,51 @@ Orbital.prototype.resources = function(r) {
 	return r;
 }
 
+/**
+ * The orbit itself.
+ */
+var orbitalIdCounter = 0;
+var orbitalId = function() {
+	return orbitalIdCounter++;
+}
+
+var Orbit = function(system) {
+	this._system = system;
+	this._orbitals = {};
+	this.init();
+}
+Orbit.prototype.init = function(){}
+
+featurify(Orbit);
+
+Orbit.prototype.system = function(){return this._system;};
+Orbit.prototype.addOrbital = function(orb){this._orbitals[orbitalId()] = orb; return this;};
+Orbit.prototype.removeOrbital = function(orb){for(var i in this._orbitals) if(orb == this._orbitals[i]) delete this._orbitals[i]; return this;}
+Orbit.prototype.orbitals = function(){var r = [];for(var k in this._orbitals) r.push(this._orbitals[k]); return r;};
+
+/**
+ * Determines the resources of this orbit.
+ */
+Orbit.prototype.resources = function(r) {
+	if(typeof r == 'undefined') r = {};
+	var fs = this.features();
+	for(var i in fs) {
+		var f = fs[i];
+		var c = f.resources(this.orbit().system(), this.orbit());
+		for(var x in c) {
+			if(typeof r[x] == 'undefined') r[x]=0;
+			r[x] += c[x];
+		}
+	}
+	for(var k in this._orbitals) {
+		this._orbitals[k].resources(r);
+	}
+	return r;
+}
+
 
 fwurg.system = {
+	Orbit: Orbit,
 	Orbital: Orbital,
 	Feature: Feature
 };
