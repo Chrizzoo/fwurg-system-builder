7302ad1402f39aedefd38d7d492eaaa5bcc36898
Chrizzoo <chrisbroekema@gmail.com>
2012-07-26 15:06:18 +0200

    split resources in cost and benefit function
    resources now makes a call to benefit and cost. these functions go all
    the way down the tree.

--- model.js
+++ model.js
@@ -32,19 +32,58 @@ Feature.prototype.isA = function(f){return f in this._classes;};
 Feature.prototype.classes = function(){var r=[];for(var f in this._classes) r.push(f);return r;}
 Feature.prototype.data = function(d,v) {if(typeof v=='undefined')return this._data[d];this._data[d]=v;return this;}
 
+var resourcesPartial = function(function_val) {
+	return function(system, orbit, orbital) {
+		if(typeof this.data(function_val) == 'function') {
+			return this.data(function_val).apply(this,[system, orbit, orbital]);
+		} else if(typeof this.data(function_val) == 'object') {
+			return this.data(function_val);
+		} else {
+			return {};
+		}
+	};
+}
+
+var mergeResourceObjects = function(obj1, obj2) {
+	var r = {};
+	for(var x in obj1) {
+		if(typeof r[x] == 'undefined') r[x]=0;
+		r[x] += obj1[x];
+	}
+	for(var x in obj2) {
+		if(typeof r[x] == 'undefined') r[x]=0;
+		r[x] += obj2[x];
+	}
+	return r;
+}
+
 /**
  * Determines the cost of the feature, based on custom data and
  * the context of use.
  */
-Feature.prototype.resources = function(system, orbit, orbital) {
-	if(typeof this.data('__resources') == 'function') {
-		return this.data('__resources').apply(this,[system, orbit, orbital]);
-	} else if(typeof this.data('__resources') == 'object') {
-		return this.data('__resources');
-	} else {
-		return {};
-	}
+Feature.prototype.cost = function(system, orbit, orbital) {
+	return resourcesPartial('__resources_cost')(system, orbit, orbital);
 }
+
+/**
+ * Determines the benefit of the feature, based on custom data and
+ * the context of use.
+ */
+Feature.prototype.benefit = function(system, orbit, orbital) {
+	return resourcesPartial('__resources_benefit')(system, orbit, orbital);
+}
+
+/**
+ * Determines the cost and benefit of the feature, based on custom data and
+ * the context of use.
+ */
+Feature.prototype.resources = function(system, orbit, orbital) {
+	var cost = this.cost(system, orbit, orbital);
+	var benefit = this.benefit(system, orbit, orbital);
+	return mergeResourceObjects(cost, benefit);
+}
+
+
 
 /**
  * Adds the feature aspect to a type.
@@ -97,22 +136,38 @@ Orbital.prototype.image = function(v){if(typeof v=='undefined')return this._imag
 Orbital.prototype.description = function(v){if(typeof v=='undefined')return this._description; this._description=v; return this;};
 Orbital.prototype.orbit = function(){return this._orbit;};
 
+var orbitalResourcesPartial = function(function_val) {
+	return function(r) {
+		if(typeof r == 'undefined') r = {};
+		var fs = this.features();
+		for(var i in fs) {
+			var f = fs[i];
+			var fn = f[function_val];
+			var c = fn(this.orbit().system(), this.orbit(), this);			
+			for(var x in c) {
+				if(typeof r[x] == 'undefined') r[x]=0;
+				r[x] += c[x];
+			}
+		}
+		return r;
+	};
+}
+
+Orbital.prototype.cost = function(r) {
+	return orbitalResourcesPartial('cost')(r);
+}
+
+Orbital.prototype.benefit = function(r) {
+	return orbitalResourcesPartial('benefit')(r);
+}
 
 /**
  * Determines the resources of this orbital.
  */
 Orbital.prototype.resources = function(r) {
-	if(typeof r == 'undefined') r = {};
-	var fs = this.features();
-	for(var i in fs) {
-		var f = fs[i];
-		var c = f.resources(this.orbit().system(), this.orbit(), this);
-		for(var x in c) {
-			if(typeof r[x] == 'undefined') r[x]=0;
-			r[x] += c[x];
-		}
-	}
-	return r;
+	var cost = this.cost(r);
+	var benefit = this.benefit(r);
+	return mergeResourceObjects(cost, benefit);
 }
 
 /**
@@ -140,24 +195,44 @@ Orbit.prototype.addOrbital = function(orb){this._orbitals[orbitalId()] = orb; re
 Orbit.prototype.removeOrbital = function(orb){for(var i in this._orbitals) if(orb == this._orbitals[i]) delete this._orbitals[i]; return this;}
 Orbit.prototype.orbitals = function(){var r = [];for(var k in this._orbitals) r.push(this._orbitals[k]); return r;};
 
+
+var orbitResourcesPartial = function(function_val) {
+	return function(r) {
+		if(typeof r == 'undefined') r = {};
+		var fs = this.features();
+		for(var i in fs) {
+			var f = fs[i];
+			var fn = f[function_val];
+			var c = fn(this.orbit().system(), this.orbit());
+			for(var x in c) {
+				if(typeof r[x] == 'undefined') r[x]=0;
+				r[x] += c[x];
+			}
+		}
+		for(var k in this._orbitals) {
+			var fn = this._orbitals[k][function_val];
+			fn(r);
+		}
+		return r;
+	};
+}
+
+Orbit.prototype.cost = function(r) {
+	return orbitResourcesPartial('cost')(r);
+}
+
+Orbit.prototype.benefit = function(r) {
+	return orbitResourcesPartial('benefit')(r);
+}
+
+
 /**
  * Determines the resources of this orbit.
  */
 Orbit.prototype.resources = function(r) {
-	if(typeof r == 'undefined') r = {};
-	var fs = this.features();
-	for(var i in fs) {
-		var f = fs[i];
-		var c = f.resources(this.orbit().system(), this.orbit());
-		for(var x in c) {
-			if(typeof r[x] == 'undefined') r[x]=0;
-			r[x] += c[x];
-		}
-	}
-	for(var k in this._orbitals) {
-		this._orbitals[k].resources(r);
-	}
-	return r;
+	var cost = this.cost(r);
+	var benefit = this.benefit(r);
+	return mergeResourceObjects(cost, benefit);
 }
 
 
