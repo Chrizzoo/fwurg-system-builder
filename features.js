501493dd3b386a54c18758200c144a9619d6a73b
Brend Wanders <b.wanders@xs4all.nl>
2012-07-30 17:44:01 +0000

    Shorthanded atmos4.

--- features.js
+++ features.js
@@ -73,35 +73,36 @@
 		};
 	}
 
-	var atmos4 = new F("rules:type_iv_atmosphere", ["biosphere"]);
+	var atmos4 = new F("rules:type_iv_atmosphere", ["biosphere"])
+    	.data('_cost', biosphereCost(1))
+    	.data('_benefit', defaultBenefit)
+    	.data('image', "rules:type_iv_atmosphere.png")
+    	.data('name', "Type IV Atmosphere");
+
 	var atmos3 = new F("rules:type_iii_atmosphere", ["biosphere"]);
 	var atmos2 = new F("rules:type_ii_atmosphere", ["biosphere"]);
 	var atmos1 = new F("rules:type_i_atmosphere", ["biosphere"]);
 	var natural_life = new F("rules:natural_life", ["biosphere"]);
 	var oceans = new F("rules:oceans", ["biosphere"]);
 
-	atmos4.data('_cost', biosphereCost(1));	
 	atmos3.data('_cost', biosphereCost(2));	
 	atmos2.data('_cost', biosphereCost(3));	
 	atmos1.data('_cost', biosphereCost(4));	
 	natural_life.data('_cost', biosphereCost(1));	
 	oceans.data('_cost', biosphereCost(1));
 
-	atmos4.data('_benefit', defaultBenefit);
 	atmos3.data('_benefit', defaultBenefit);
 	atmos2.data('_benefit', defaultBenefit);
 	atmos1.data('_benefit', defaultBenefit);
 	natural_life.data('_benefit', defaultBenefit);
 	oceans.data('_benefit', defaultBenefit);
 
-	atmos4.data('image', "rules:type_iv_atmosphere.png");
 	atmos3.data('image', "rules:type_iv_atmosphere.png");
 	atmos2.data('image', "rules:type_ii_atmosphere.png");
 	atmos1.data('image', "rules:type_i_atmosphere.png");
 	natural_life.data('image', "");
 	oceans.data('image', "rules:water_ocean.png");
 
-	atmos4.data('name', "Type IV Atmosphere");
 	atmos3.data('name', "Type III Atmosphere");
 	atmos2.data('name', "Type II Atmosphere");
 	atmos1.data('name', "Type I Atmosphere");
