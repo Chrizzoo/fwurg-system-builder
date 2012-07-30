c088e6f8d6cca3aef545188d62797ea5191473ee
Chrizzoo <chrisbroekema@gmail.com>
2012-07-30 19:00:06 +0200

    star apply function
    function to create a system model with the orbits that the star allows.

--- datamodel.js
+++ datamodel.js
@@ -10,21 +10,7 @@ fwurg.system.init = function () {
 		'bio mass': 200
 	};
 	
-	var system = new fwurg.system.System();
-	
-	// alias the Orbit constructor
-	var O = fwurg.system.Orbit;
-
-	system.star(new O(system));
-	system.star().addFeature("rules:orange_giant_star");
-	
-	system.addOrbit(new O(system).addFeature("rules:hot_orbit"));
-	system.addOrbit(new O(system).addFeature("rules:hot_orbit"));
-	system.addOrbit(new O(system).addFeature("rules:goldilocks_orbit"));
-	system.addOrbit(new O(system).addFeature("rules:goldilocks_orbit"));
-	system.addOrbit(new O(system).addFeature("rules:cold_orbit"));
-	system.addOrbit(new O(system).addFeature("rules:cold_orbit"));
-	system.addOrbit(new O(system).addFeature("rules:cold_orbit"));
+	var system = fwurg.system.applyStar("rules:orange_giant_star");
 	
 	var orbit_0 = system.Orbits()[0];
 	var giant = new fwurg.system.Orbital(orbit_0);
@@ -45,8 +31,38 @@ fwurg.system.init = function () {
 	
 }
 
-
-
-
+/**
+  * Apply a star feature, creating a new system with the necessary orbits.
+  */
+fwurg.system.applyStar = function(starFeature) {
+	// alias the Orbit constructor
+	var O = fwurg.system.Orbit;
+	
+	// create a clean system.
+	var s = new fwurg.system.System();
+	
+	// add the star feature to the star orbit.
+	s.star(new O(s).addFeature(starFeature));
+	
+	var f = fwurg.system.Feature.get(starFeature);
+	var orbitresources = f.benefit();
+	
+	if(typeof orbitresources['hot orbits'] != 'undefined') {
+		for(i = 0; i < orbitresources['hot orbits']; i++) {
+			s.addOrbit(new O(s).addFeature("rules:hot_orbit"));
+		}
+	}
+	if(typeof orbitresources['goldilocks orbits'] != 'undefined') {
+		for(i = 0; i < orbitresources['goldilocks orbits']; i++) {
+			s.addOrbit(new O(s).addFeature("rules:goldilocks_orbit"));
+		}
+	}
+	if(typeof orbitresources['cold orbits'] != 'undefined') {
+		for(i = 0; i < orbitresources['cold orbits']; i++) {
+			s.addOrbit(new O(s).addFeature("rules:cold_orbit"));
+		}
+	}
+	return s;
+}
 
 })(fwurg);
